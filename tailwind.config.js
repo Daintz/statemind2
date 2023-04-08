6a0284129dda432293b1ebb228c640ed706cadb3
D4intzz <81586667+Daintz@users.noreply.github.com>
2023-04-08 09:57:54 -0500

    style: :construction: add contact and footer
    add contact, footer and fix responsive on landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      lgm: { max: "1023px" },
+      ...defaultTheme.screens,
+    },
   },
   plugins: [],
 };
