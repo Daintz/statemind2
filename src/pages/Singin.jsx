de8f182b2fcd13fa3fc97afa591c78ce173cd8a3
D4intzz <81586667+Daintz@users.noreply.github.com>
2023-04-08 14:51:48 -0500

    style: :construction: add login and register
    Add login and register in this project

--- src/pages/Singin.jsx
+++ src/pages/Singin.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
 
 const Singin = () => {
   return (
-    <div>Singin</div>
+    <form className="lgm:px-[10%] lg:px-[27%] pt-[8rem] pb-[9.3rem]">
+      <div className="p-12 rounded-lg bg-white border border-black">
+        <h1 className="text-4xl font-bold text-center mb-6">LOGIN</h1>
+        <div className="mb-6">
+          <label
+            htmlFor="email"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            placeholder="correo@correo.com"
+            required
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            htmlFor="password"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            placeholder="*********"
+            required
+          />
+        </div>
+      <button
+        type="submit"
+        className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+      >
+        Register new account
+      </button>
+      </div>
+    </form>
   )
 }
 
--- src/pages/Singup.jsx
+++ src/pages/Singup.jsx
@@ -1,7 +1,92 @@
 
 function Singup () {
   return (
-    <div>SingUp</div>
+    <form className="lgm:px-[10%] lg:px-[27%] pt-[3.5rem] pb-[5.5rem]">
+      <div className="p-12 rounded-lg bg-white border border-black">
+        <h1 className="text-4xl font-bold text-center mb-6">REGISTER</h1>
+        <div className="mb-6">
+          <label
+            htmlFor="name"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            placeholder="Daniel Lopez Rubio"
+            required
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            htmlFor="age"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            Age
+          </label>
+          <input
+            type="text"
+            id="age"
+            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            placeholder="19"
+            required
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            htmlFor="email"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            placeholder="correo@correo.com"
+            required
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            htmlFor="password"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            placeholder="*********"
+            required
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            htmlFor="repeat-password"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            Repeat password
+          </label>
+          <input
+            type="password"
+            id="repeat-password"
+            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            placeholder="*********"
+            required
+          />
+        </div>
+      <button
+        type="submit"
+        className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+      >
+        Register new account
+      </button>
+      </div>
+    </form>
   )
 }
 
